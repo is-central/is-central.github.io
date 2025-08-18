48848a44d7c3959719c3dedad9fae6aa51e640d8
Jinyue Liu <ericliu.elel@gmail.com>
2025-08-18 10:48:27 -0700

    feat: Redesign homepage and structure content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,55 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: 'https://is-central.github.io',
+
+  integrations: [
+    starlight({
+      title: 'IS Central',
+      logo: {
+        src: 'src/assets/logo.svg',
+      },
+      social: {
+        discord: 'https://discord.gg/fqRBzQWVfK',
+      },
+      sidebar: [
+        {
+          label: 'Introduction',
+          link: '/introduction/',
+        },
+        {
+          label: 'Phantom & Crimson Solitaire (IS2)',
+          items: [
+            { label: 'Overview', link: '/is2-phantom/overview/' },
+          ],
+        },
+        {
+          label: 'Mizuki & Caerula Arbor (IS3)',
+          items: [
+            { label: 'Overview', link: '/is3-mizuki/overview/' },
+          ],
+        },
+        {
+          label: 'Expeditioner\'s Joklumarkar (IS4)',
+          items: [
+            { label: 'Overview', link: '/is4-expeditioners/overview/' },
+          ],
+        },
+        {
+          label: 'Sarkaz\'s Furnaceside Fables (IS5)',
+          items: [
+            { label: 'Overview', link: '/is5-sarkaz/overview/' },
+          ],
+        },
+        {
+          label: 'Sui\'s Garden of Grotesqueries (IS6)',
+          items: [
+            { label: 'Overview', link: '/is6-sui/overview/' },
+          ],
+        },
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/is5-sarkaz/overview.md
@@ -0,0 +1,6 @@
+---
+title: IS2 Overview
+description: An overview of Phantom & Crimson Solitaire.
+---
+
+Content for IS2 is coming soon!
