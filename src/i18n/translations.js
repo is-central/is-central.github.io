95e71ceec6c6e39c91f5ba14ba73e665d2bf4d8e
DieAliveRs <77551462+DieAliveRs@users.noreply.github.com>
2025-11-09 21:07:38 -0800

    vn translation (#81)

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -1,6 +1,7 @@
 export const translations = {
   "kazdel-kasino-2": {
     en: "/events/kazdel-kasino-2",
+    vn: "/events/kazdel-kasino-2-vn",
     jp: "/events/kazdel-kasino-2-jp",
     kr: "/events/kazdel-kasino-2-kr"
   }
