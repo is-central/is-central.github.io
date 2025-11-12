acd8d69c1beb5f8321242cdb1d9dce787f6f1d25
DieAliveRs <77551462+DieAliveRs@users.noreply.github.com>
2025-11-11 21:03:42 -0800

    sc and tc translations added (#83)

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -3,6 +3,8 @@ export const translations = {
     en: "/events/kazdel-kasino-2",
     vn: "/events/kazdel-kasino-2-vn",
     jp: "/events/kazdel-kasino-2-jp",
-    kr: "/events/kazdel-kasino-2-kr"
+    kr: "/events/kazdel-kasino-2-kr",
+    tc: "/events/kazdel-kasino-2-tc",
+    sc: "/events/kazdel-kasino-2-sc"
   }
 };
