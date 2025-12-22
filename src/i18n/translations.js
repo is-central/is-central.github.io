80386a646ac6832730cc41d5e5ef5bcdd810572d
DieAliveRs <77551462+DieAliveRs@users.noreply.github.com>
2025-12-21 16:25:32 -0800

    style changes for events page

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -1,10 +1,10 @@
 export const translations = {
   "kazdel-kasino-2": {
-    en: "/events/kazdel-kasino-2",
-    vn: "/events/kazdel-kasino-2-vn",
-    jp: "/events/kazdel-kasino-2-jp",
-    kr: "/events/kazdel-kasino-2-kr",
-    tc: "/events/kazdel-kasino-2-tc",
-    sc: "/events/kazdel-kasino-2-sc"
+    en: "/events/kazdel-kasino/kazdel-kasino-2-rules/kazdel-kasino-2",
+    vn: "/events/kazdel-kasino/kazdel-kasino-2-rules/kazdel-kasino-2-vn",
+    jp: "/events/kazdel-kasino/kazdel-kasino-2-rules/kazdel-kasino-2-jp",
+    kr: "/events/kazdel-kasino/kazdel-kasino-2-rules/kazdel-kasino-2-kr",
+    tc: "/events/kazdel-kasino/kazdel-kasino-2-rules/kazdel-kasino-2-tc",
+    sc: "/events/kazdel-kasino/kazdel-kasino-2-rules/kazdel-kasino-2-sc"
   }
 };
